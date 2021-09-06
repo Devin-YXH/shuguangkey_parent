e7179db7cea00fcaa8a299a78c9c37b62eabe525
devin <1103560785@qq.com>
2021-09-06 20:21:40 +0800

    增加 分页查询讲师列表

--- service/service_edu/src/main/java/com/shiguangkey/eduservice/config/MyBatisPlusConfig.java
+++ service/service_edu/src/main/java/com/shiguangkey/eduservice/config/MyBatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.shiguangkey.eduservice.config;
 
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,5 +21,12 @@ public class MyBatisPlusConfig {
         return new LogicSqlInjector();
     }
 
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
 
 }
--- service/service_edu/src/main/java/com/shiguangkey/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/shiguangkey/eduservice/controller/EduTeacherController.java
@@ -1,8 +1,13 @@
 package com.shiguangkey.eduservice.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.shiguangkey.commonutils.R;
 import com.shiguangkey.eduservice.entity.EduTeacher;
 import com.shiguangkey.eduservice.service.EduTeacherService;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
 import org.aspectj.weaver.ast.Var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +22,7 @@ import java.util.List;
  * @author testjava
  * @since 2021-09-03
  */
+
 @RestController
 @RequestMapping("/eduservice/eduteacher")
 public class EduTeacherController {
@@ -28,6 +34,7 @@ public class EduTeacherController {
      * 查询所有讲师
      * @return
      */
+    @ApiOperation(value = "查询所有讲师")
     @GetMapping
     public List<EduTeacher> getAllTeacher(){
         List<EduTeacher> teacherList = teacherService.list(null);
@@ -39,10 +46,21 @@ public class EduTeacherController {
      * @param id
      * @return
      */
+    @ApiOperation(value = "根据ID逻辑删除讲师")
     @DeleteMapping("{id}")
     public boolean delTeacher(@PathVariable String id){
         boolean remove = teacherService.removeById(id);
         return remove;
+    }
+
+    @ApiOperation(value = "分页查询讲师列表")
+    @GetMapping("getTeacherPage/{current}/{limt}")
+    public R getTeacherPage(@PathVariable Long current,@PathVariable Long limt){
+        Page<EduTeacher> page = new Page<>(current, limt);
+        List<EduTeacher> records = page.getRecords();
+        long total = page.getTotal();
+
+        return R.ok().data("list",records).data("total",total);
     }
 
 }
