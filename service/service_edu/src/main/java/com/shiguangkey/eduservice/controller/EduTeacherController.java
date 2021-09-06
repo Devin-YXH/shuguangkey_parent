0406738726f961322662b51ab5e723cf1fefad7c
devin <1103560785@qq.com>
2021-09-06 21:31:42 +0800

    增加 带条件的分页查询讲师数据 有BUG

--- service/service_edu/src/main/java/com/shiguangkey/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/shiguangkey/eduservice/controller/EduTeacherController.java
@@ -1,28 +1,26 @@
 package com.shiguangkey.eduservice.controller;
 
-
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.shiguangkey.commonutils.R;
 import com.shiguangkey.eduservice.entity.EduTeacher;
+import com.shiguangkey.eduservice.entity.vo.TeacherQuery;
 import com.shiguangkey.eduservice.service.EduTeacherService;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.aspectj.weaver.ast.Var;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 /**
- * <p>
  * 讲师 前端控制器
- * </p>
  *
  * @author testjava
  * @since 2021-09-03
  */
 
+@Api(description="讲师管理")
 @RestController
 @RequestMapping("/eduservice/eduteacher")
 public class EduTeacherController {
@@ -53,10 +51,51 @@ public class EduTeacherController {
         return remove;
     }
 
+    /**
+     * 分页查询讲师列表
+     * @param current
+     * @param limt
+     * @return
+     */
     @ApiOperation(value = "分页查询讲师列表")
     @GetMapping("getTeacherPage/{current}/{limt}")
     public R getTeacherPage(@PathVariable Long current,@PathVariable Long limt){
         Page<EduTeacher> page = new Page<>(current, limt);
+        teacherService.page(page, null);
+        List<EduTeacher> records = page.getRecords();
+        long total = page.getTotal();
+        return R.ok().data("list",records).data("total",total);
+    }
+
+    @ApiOperation(value = "带条件的分页查询讲师数据")
+    @PostMapping("getTeacherPageVo/{current}/{limit}")
+    public R getTeacherPageVo(@PathVariable Long current,
+                              @PathVariable Long limit,
+                              @RequestBody TeacherQuery teacherQuery){
+        //@RequestBody把json串转换成实体类
+        //1、取出查询的条件
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+
+        //2、判断条件是否为空，如果不为空就拼接SQL
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        if(!StringUtils.isEmpty(name)){
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)){
+            wrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(begin)){
+            wrapper.ge("gmt_create",begin);
+        }
+        if(!StringUtils.isEmpty(end)){
+            wrapper.le("gmt_create",end);
+        }
+
+        Page<EduTeacher> page = new Page<>(current,limit);
+        teacherService.page(page,wrapper);
         List<EduTeacher> records = page.getRecords();
         long total = page.getTotal();
 
